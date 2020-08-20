3ca085dab5a0b65c6dfeab75e5125c5f5ebf603f
sleepo-chin <chin@DESKTOP-36SPUK7.localdomain>
2020-08-21 00:56:43 +0700

    this source code is successed

--- jupyterhub/jupyterhub_config-success.py
+++ jupyterhub/jupyterhub_config-success.py
@@ -13,9 +13,7 @@ import os
 
 ## Generic
 c.JupyterHub.admin_access = True
-#c.Spawner.default_url = '/lab'
-c.Authenticator.admin_users = { 'admin' }
-c.DummyAuthenticator.password = "1admin@"
+c.Spawner.default_url = '/lab'
 
 ## Docker spawner
 c.JupyterHub.spawner_class = 'dockerspawner.DockerSpawner'
@@ -23,10 +21,6 @@ c.DockerSpawner.image = os.environ['DOCKER_JUPYTER_CONTAINER']
 c.DockerSpawner.network_name = os.environ['DOCKER_NETWORK_NAME']
 # See https://github.com/jupyterhub/dockerspawner/blob/master/examples/oauth/jupyterhub_config.py
 c.JupyterHub.hub_ip = os.environ['HUB_IP']
-
-c.JupyterHub.shutdown_on_logout = True
-
-c.LocalAuthenticator.create_system_users = True
 
 # user data persistence
 # see https://github.com/jupyterhub/dockerspawner#data-persistence-and-dockerspawner
